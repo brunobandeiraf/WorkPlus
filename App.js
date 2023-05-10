f64d11a8c5939820201d5eec0c005c76cd288f94
Luigi dos Santos <114340043+LuigiInt1@users.noreply.github.com>
2023-05-10 17:44:52 -0300

    Reorganização GitHub
    Atualizando alguns arquivos e deletando outros.

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
-import React,{useState} from 'react';
-import "expo-router/entry";
-import { Text, View, StyleSheet,Image, TextInput, SafeAreaView, ScrollView, TouchableOpacity, ViewComponent} from 'react-native';
-import Constants from 'expo-constants';
-import { Button, Input, Text } from "react-native";
-import { createStackNavigator } from '@react-navigation/stack';
+import * as React from 'react';
+import { Button, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import minhasOfertas from './src/views/minhasOfertas';
-import minhasPrestacoes from './src/views/minhasPrestacoes';
-import { paginaPrincipal } from './src/views/paginaPrincipal';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './paginas/Home';
+import Servico from './paginas/Servico';
+import Trabalho from './paginas/Trabalho';
+const Stack = createStackNavigator()
 
-const Stack = createStackNavigator();
 
-export default function MyStack() {
-  return (
-
+export default function App(){
+return(
+  <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name="minhasOfertas" component={minhasOfertas} />
-      <Stack.Screen name="Principal" component={minhasPrestacoes} />
-      <Stack.Screen name="PaginaPrincipal" component={paginaPrincipal} />
+      <Stack.Screen name="Home" component={Home} options={{ headerShown: false}} />
+      <Stack.Screen name="Servico" component={Servico} options={{ headerShown: false}} />
+      <Stack.Screen name="Trabalho" component={Trabalho} options={{ headerShown: false}} />
     </Stack.Navigator>
-  );
-}
+  </NavigationContainer>
+)}
